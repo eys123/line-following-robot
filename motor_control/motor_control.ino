d05980972abd51a8adb655ab4411aa51662a7583
eys123 <esun@olin.edu>
2023-10-05 13:41:11 -0400

    Set up basic motor control

---
+++ motor_control/motor_control.ino
@@ -0,0 +1,16 @@
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);
+
+void setup() {
+  AFMS.begin();
+  rightMotor->setSpeed(150);
+}
+
+void loop() {
+  rightMotor->run(BACKWARD);
+}
