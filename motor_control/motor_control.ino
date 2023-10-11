65ec0af3d70e03c7538f5e2f45901319f5d3df33
eys123 <esun@olin.edu>
2023-10-11 15:18:04 -0400

    Adjust motor code to work for track

--- motor_control/motor_control.ino
+++ motor_control/motor_control.ino
@@ -6,37 +6,47 @@ Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
 Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);
 // is wired in reverse; has to run BACKWARD to go forwards
 
-const int leftSensorPin = A0;
-const int rightSensorPin = A1;
+const int leftSensorPin = A1;
+const int rightSensorPin = A0;
+const int midRightSensorPin = A2;
+const int midLeftSensorPin = A3;
 int leftSensorVal = 0;
 int rightSensorVal = 0;
-int sensorThreshold = 820;
+int midLeftSensorVal = 0;
+int midRightSensorVal = 0;
+int sensorThreshold = 650;
+int turnSpeed = 20;
+int forwardSpeed = 30;
 
 void setup() {
   Serial.begin(9600);
   AFMS.begin();
-  rightMotor->setSpeed(50);
-  leftMotor->setSpeed(50);
 }
 
 void loop() {
+  rightMotor->setSpeed(turnSpeed);
+  leftMotor->setSpeed(turnSpeed);
   readSensorInfo();
   // check both; if both are aight then move forward
-  if (leftSensorVal > sensorThreshold && rightSensorVal > sensorThreshold) {
-    leftMotor -> run(FORWARD);
-    leftMotor -> run(BACKWARD);
+  if (leftSensorVal < sensorThreshold && rightSensorVal < sensorThreshold && (midLeftSensorVal > sensorThreshold || midRightSensorVal > sensorThreshold)) {
+    rightMotor->setSpeed(forwardSpeed);
+    leftMotor->setSpeed(forwardSpeed);
+    forward();
   }
   else if (leftSensorVal > sensorThreshold) {
-    leftMotor -> run(RELEASE);
-    rightMotor -> run(BACKWARD);
+    turnLeft();
   }
   else if (rightSensorVal > sensorThreshold) {
-    leftMotor -> run(FORWARD);
-    rightMotor -> run(RELEASE);
+    turnRight();
   }
-  else {
+  else if (leftSensorVal < sensorThreshold && rightSensorVal < sensorThreshold && midLeftSensorVal < sensorThreshold && midRightSensorVal < sensorThreshold) {
     leftMotor -> run(FORWARD);
-    rightMotor -> run(FORWARD);
+    rightMotor -> run(BACKWARD);
+  }
+  else if (leftSensorVal > sensorThreshold && rightSensorVal > sensorThreshold) {
+    rightMotor->setSpeed(30);
+    leftMotor->setSpeed(30);
+    turnRight();
   }
   // wait 2 milliseconds before the next loop for the analog-to-digital
   // converter to settle after the last reading:
@@ -47,10 +57,33 @@ void readSensorInfo() {
   // read the analog in value:
   leftSensorVal = analogRead(leftSensorPin);
   rightSensorVal = analogRead(rightSensorPin);
+  midLeftSensorVal = analogRead(midLeftSensorPin);
+  midRightSensorVal = analogRead(midRightSensorPin);
   // print the results to the Serial Monitor:
   Serial.print("[");
   Serial.print(leftSensorVal);
   Serial.print(", ");
+  Serial.print(midLeftSensorVal);
+  Serial.print(", ");
+  Serial.print(midRightSensorVal);
+  Serial.print(", ");
   Serial.print(rightSensorVal);
   Serial.println("]");
+}
+
+void turnLeft() {
+  leftMotor -> run(FORWARD);
+  rightMotor -> run(FORWARD);
+  Serial.println("TURN LEFT");
+}
+
+void turnRight() {
+  leftMotor -> run(BACKWARD);
+  rightMotor -> run(BACKWARD);
+  Serial.println("TURN RIGHT");
+}
+
+void forward() {
+  leftMotor -> run(BACKWARD);
+  rightMotor -> run(FORWARD);
 }
